9003d4b9c13d3824ef513b830a458f23b0596eae
Ingo Dahn <dahn@dahn-research.eu>
2023-04-01 20:55:24 +0200

    Locale switcher working

--- src/i18n/translation.js
+++ src/i18n/translation.js
@@ -1,7 +1,16 @@
+import i18n from "@/i18n"
+
 const Trans = {
     get supportedLocales() {
         return import.meta.env.VITE_SUPPORTED_LOCALES.split(",")
-    }
+    },
+    set currentLocale(newLocale) { // set the current locale
+        i18n.global.locale.value = newLocale
+      },
+      async switchLanguage(newLocale) { // switch the current locale
+        Trans.currentLocale = newLocale
+        document.querySelector("html").setAttribute("lang", newLocale)
+      }
 }
 
 export default Trans
