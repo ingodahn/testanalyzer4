82d593394f97609f9c5dd400531031ac9ec0abd7
Ingo Dahn <dahn@dahn-research.eu>
2023-04-06 14:36:28 +0200

    Added v-menu for language switching in appbar

--- src/components/LanguageSwitcher.vue
+++ src/components/LanguageSwitcher.vue
@@ -1,37 +1,119 @@
 <template>
-    <select @change="switchLanguage">
-        <option v-for="sLocale in supportedLocales" :key="`locale-${sLocale}`" :value="sLocale"
-            :selected="locale === sLocale">
-            {{ t(`locale.${sLocale}`) }}
-        </option>
-    </select>
-</template>
+  <div>   
+    <v-menu offset-y>
+      <template v-slot:activator="{ props }">
+        <v-btn color="primary"  v-bind="props">🌐</v-btn>
+      </template>
 
+      <v-list>
+        <v-list-item v-for="(item, index) in items" :key="index" :value=item.value @click="selectLanguage(item)">
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+
+    
+    <select @change="switchLanguage">
+      <option v-for="sLocale in supportedLocales" :key="`locale-${sLocale}`" :value="sLocale"
+        :selected="locale === sLocale">
+        {{ t(`locale.${sLocale}`) }}
+      </option>
+    </select>
+    <!--
+    <v-menu v-model="selectedLanguage">
+      <template v-slot:activator="{ on }">
+        <v-btn dark color="primary" v-on="on || {}">
+          🌐
+        </v-btn>
+      </template>
+      <v-list>
+        <v-list-item
+          v-for="sLocale in supportedLocales"
+          :key="`locale-${sLocale}`" :value="sLocale"
+          @click="switchLanguage"
+        >
+          
+            <v-list-item-title>{{ t(`locale.${sLocale}`) }}</v-list-item-title>
+          
+        </v-list-item>
+      </v-list>
+    </v-menu>
+  -->
+  </div>
+</template>
 <script>
 import { useI18n } from 'vue-i18n'
 import { useRouter } from "vue-router"
 import Tr from "@/i18n/translation"
 
 export default {
-    setup() {
-        const { t, locale } = useI18n()
-
-        const supportedLocales = Tr.supportedLocales
-        const router = useRouter()
-
-        const switchLanguage = async (event) => {
-            const newLocale = event.target.value
-
-            await Tr.switchLanguage(newLocale)
-
-            try {
-                await router.replace({ params: {locale: newLocale }})
-            } catch(e) {
-                router.push("/")
-            }
-        }
-
-        return { t, locale, supportedLocales, switchLanguage }
+  data() {
+    return {
+      selectedLanguage: 'de',
+      
+      items: [
+        { title: 'English', value: 'en' },
+        { title: 'Deutsch', value: 'de' },
+      ],
+      
+      //items: ['Item 1', 'Item 2', 'Item 3'],
     }
+  },
+  methods: {
+    async selectLanguage(item)  {
+      console.log(`Selected item ${item.value}`);
+      const newLocale = item.value
+      await Tr.switchLanguage(newLocale)
+      /*
+      try {
+        await router.replace({ params: { locale: newLocale } })
+      } catch (e) {
+        router.push("/")
+      }
+      */
+    },
+  },
+  setup() {
+
+    const { t, locale } = useI18n()
+
+    const supportedLocales = Tr.supportedLocales
+    const router = useRouter()
+
+    methods: [
+      switchLanguage2
+    ]
+    function switchLanguage2() {
+      console.log(this.selectedLanguage)
+    }
+    const switchLanguage1 = async (event) => {
+      console.log(event.target.value)
+
+      const newLocale = this.selectedLanguage
+
+      await Tr.switchLanguage(newLocale)
+
+      try {
+        await router.replace({ params: { locale: newLocale } })
+      } catch (e) {
+        router.push("/")
+      }
+    }
+
+    const switchLanguage = async (event) => {
+
+      const newLocale = event.target.value
+
+      await Tr.switchLanguage(newLocale)
+
+      try {
+        await router.replace({ params: { locale: newLocale } })
+      } catch (e) {
+        router.push("/")
+      }
+    }
+
+    return { t, locale, supportedLocales, switchLanguage }
+  }
 }
 </script>
--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,19 +1,55 @@
 <template>
+  <div>
     <v-app-bar app color="primary" class="px-10" dark>
       <div class="d-flex align-center">
         <h2>My Company {{ $t("Test.title") }}</h2>
       </div>
-  
+
       <v-spacer></v-spacer>
-  
+
       <v-btn href="https://mycompany.com/contact-us" target="_blank" text>
         <span class="mr-2">Contact US</span>
         <v-icon>mdi-open-in-new</v-icon>
       </v-btn>
     </v-app-bar>
-  </template>
+    <language-switcher />
+
+    <v-menu offset-y>
+      <template v-slot:activator="{ props }">
+        <v-btn color="primary" dark v-bind="props">Menu</v-btn>
+      </template>
+
+      <v-list>
+        <v-list-item v-for="(item, index) in items" :key="index" @click="selectItem(item)">
+          <v-list-item-title>{{ item }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+  </div>
+</template>
   
-  <script setup>
-  //
-  </script>
+<script>
+import LanguageSwitcher from "@/components/LanguageSwitcher.vue"
+export default {
+  data: () => ({
+    /*
+    items: [
+      { title: 'Click Me' },
+      { title: 'Click Me' },
+      { title: 'Click Me' },
+      { title: 'Click Me 2' },
+    ],
+    */
+    items: ['Item 1', 'Item 2', 'Item 3'],
+  }),
+  components: {
+    LanguageSwitcher
+  },
+  methods: {
+    selectItem(item) {
+      console.log(`Selected item ${item}`);
+    },
+  },
+}
+</script>
   
