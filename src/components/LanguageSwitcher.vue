18acd6932f64cc0f981087e5bd2908e532ed21dd
Ingo Dahn <dahn@dahn-research.eu>
2023-04-06 16:00:26 +0200

    Cleaning up

--- src/components/LanguageSwitcher.vue
+++ src/components/LanguageSwitcher.vue
@@ -1,68 +1,50 @@
 <template>
-  <div>   
+  <div>
     <v-menu offset-y>
       <template v-slot:activator="{ props }">
-        <v-btn color="primary"  v-bind="props">🌐</v-btn>
+        <v-btn color="primary" v-bind="props"><v-icon icon="mdi-web" color="white"/></v-btn>
       </template>
 
       <v-list>
-        <v-list-item v-for="(item, index) in items" :key="index" :value=item.value @click="selectLanguage(item)">
-          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        <v-list-item v-for="sLocale in newLocales" :key="`locale-${sLocale}`" :value="sLocale"
+          @click="selectLanguage(sLocale)">
+          <v-list-item-title>{{ t(`locale.${sLocale}`) }}</v-list-item-title>
         </v-list-item>
       </v-list>
     </v-menu>
 
-    
+    <!--
     <select @change="switchLanguage">
       <option v-for="sLocale in supportedLocales" :key="`locale-${sLocale}`" :value="sLocale"
         :selected="locale === sLocale">
         {{ t(`locale.${sLocale}`) }}
       </option>
     </select>
-    <!--
-    <v-menu v-model="selectedLanguage">
-      <template v-slot:activator="{ on }">
-        <v-btn dark color="primary" v-on="on || {}">
-          🌐
-        </v-btn>
-      </template>
-      <v-list>
-        <v-list-item
-          v-for="sLocale in supportedLocales"
-          :key="`locale-${sLocale}`" :value="sLocale"
-          @click="switchLanguage"
-        >
-          
-            <v-list-item-title>{{ t(`locale.${sLocale}`) }}</v-list-item-title>
-          
-        </v-list-item>
-      </v-list>
-    </v-menu>
-  -->
+    -->
+
   </div>
 </template>
 <script>
 import { useI18n } from 'vue-i18n'
-import { useRouter } from "vue-router"
+//import { useRouter } from "vue-router"
 import Tr from "@/i18n/translation"
 
 export default {
   data() {
     return {
-      selectedLanguage: 'de',
-      
-      items: [
-        { title: 'English', value: 'en' },
-        { title: 'Deutsch', value: 'de' },
-      ],
-      
-      //items: ['Item 1', 'Item 2', 'Item 3'],
+      supportedLocales: Tr.supportedLocales,
     }
   },
+  computed: {
+    newLocales() {
+      return this.supportedLocales.filter((locale) => locale !== this.locale)
+    },
+  },
   methods: {
-    async selectLanguage(item)  {
-      console.log(`Selected item ${item.value}`);
-      const newLocale = item.value
+    async selectLanguage(item) {
+      console.log('Locale:', this.locale)
+      console.log('Selected item', item);
+      const newLocale = item
       await Tr.switchLanguage(newLocale)
       /*
       try {
@@ -78,39 +60,21 @@ export default {
     const { t, locale } = useI18n()
 
     const supportedLocales = Tr.supportedLocales
-    const router = useRouter()
-
-    methods: [
-      switchLanguage2
-    ]
-    function switchLanguage2() {
-      console.log(this.selectedLanguage)
-    }
-    const switchLanguage1 = async (event) => {
-      console.log(event.target.value)
-
-      const newLocale = this.selectedLanguage
-
-      await Tr.switchLanguage(newLocale)
-
-      try {
-        await router.replace({ params: { locale: newLocale } })
-      } catch (e) {
-        router.push("/")
-      }
-    }
+    console.log('supportedLocales:', supportedLocales)
+    //const router = useRouter()
 
     const switchLanguage = async (event) => {
 
       const newLocale = event.target.value
 
       await Tr.switchLanguage(newLocale)
-
+      /*
       try {
         await router.replace({ params: { locale: newLocale } })
       } catch (e) {
         router.push("/")
       }
+      */
     }
 
     return { t, locale, supportedLocales, switchLanguage }
