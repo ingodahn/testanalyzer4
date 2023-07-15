56180f77981c6c2060e9ca5bd36becd547529281
Ingo Dahn <dahn@dahn-research.eu>
2023-07-15 21:53:15 +0200

    Source code published

--- src/components/Context.vue
+++ src/components/Context.vue
@@ -9,7 +9,7 @@
         <p>
             <b>{{ $t("Test.p3b") }}</b>
             {{ $t("Test.p3") }} (
-            <a href="https://de.wikipedia.org/wiki/Edge_Computing" target="_blank">Edge Computing</a>)
+            <a href="https://de.wikipedia.org/wiki/Edge_Computing" target="_blank">Edge Computing</a>) {{ $t("Test.p3c") }}
         </p>
         <p v-html="$t('Test.p4', ['dahn@dahn-research.eu'])"></p>
     </v-container>
